2cdff63613e406b8874157050955e58a9c7f6920
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-10-06 11:49:33 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- balloon_card.php
+++ balloon_card.php
@@ -39,17 +39,31 @@
 // Load Dolibarr environment
 $res = 0;
 // Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php");
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
+	$res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php");
+}
 // Try main.inc.php into web root detected using web root caluclated from SCRIPT_FILENAME
 $tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
 while ($i>0 && $j>0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) { $i--; $j--; }
-if (!$res && $i>0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) $res = @include(substr($tmp, 0, ($i + 1))."/main.inc.php");
-if (!$res && $i>0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) $res = @include(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php");
+if (!$res && $i>0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) {
+	$res = @include(substr($tmp, 0, ($i + 1))."/main.inc.php");
+}
+if (!$res && $i>0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) {
+	$res = @include(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php");
+}
 // Try main.inc.php using relative path
-if (!$res && file_exists("../main.inc.php")) $res = @include("../main.inc.php");
-if (!$res && file_exists("../../main.inc.php")) $res = @include("../../main.inc.php");
-if (!$res && file_exists("../../../main.inc.php")) $res = @include("../../../main.inc.php");
-if (!$res) die("Include of main fails");
+if (!$res && file_exists("../main.inc.php")) {
+	$res = @include("../main.inc.php");
+}
+if (!$res && file_exists("../../main.inc.php")) {
+	$res = @include("../../main.inc.php");
+}
+if (!$res && file_exists("../../../main.inc.php")) {
+	$res = @include("../../../main.inc.php");
+}
+if (!$res) {
+	die("Include of main fails");
+}
 
 include_once(DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php');
 dol_include_once('/flightballoon/class/bbc_ballons.class.php');
@@ -58,34 +72,34 @@ dol_include_once('/flightballoon/lib/card.lib.php');
 global $user, $langs, $db;
 
 // Load traductions files requiredby by page
-$langs->loadLangs(array("mymodule@flightballoon","other"));
+$langs->loadLangs(array("mymodule@flightballoon", "other"));
 
 // Get parameters
-$id			= GETPOST('id', 'int');
-$action		= GETPOST('action', 'alpha');
+$id = GETPOST('id', 'int');
+$action = GETPOST('action', 'alpha');
 $cancel     = GETPOST('cancel', 'aZ09');
 $backtopage = GETPOST('backtopage', 'alpha');
 
 // Initialize technical objects
-$object=new Bbc_ballons($db);
+$object = new Bbc_ballons($db);
 $extrafields = new ExtraFields($db);
-$hookmanager->initHooks(array('llx_bbc_ballonscard'));     // Note that conf->hooks_modules contains array
+$hookmanager->initHooks(array('llx_bbc_ballonscard')); // Note that conf->hooks_modules contains array
 // Fetch optionals attributes and labels
 $extralabels = $extrafields->fetch_name_optionals_label('llx_bbc_ballons');
-$search_array_options=$extrafields->getOptionalsFromPost($extralabels,'','search_');
+$search_array_options = $extrafields->getOptionalsFromPost($extralabels, '', 'search_');
 
 // Initialize array of search criterias
-$search_all=trim(GETPOST("search_all",'alpha'));
-$search=array();
-foreach($object->fields as $key => $val)
+$search_all = trim(GETPOST("search_all", 'alpha'));
+$search = array();
+foreach ($object->fields as $key => $val)
 {
-	if (GETPOST('search_'.$key,'alpha')) $search[$key]=GETPOST('search_'.$key,'alpha');
+	if (GETPOST('search_'.$key, 'alpha')) $search[$key] = GETPOST('search_'.$key, 'alpha');
 }
 
-if (empty($action) && empty($id) && empty($ref)) $action='view';
+if (empty($action) && empty($id) && empty($ref)) $action = 'view';
 
 // Protection if external user
-if ($user->societe_id > 0)
+if ($user->societe_id>0)
 {
 	//accessforbidden();
 }
@@ -103,119 +117,119 @@ include DOL_DOCUMENT_ROOT.'/core/actions_fetchobject.inc.php'; // Must be includ
  * Put here all code to do according to value of "action" parameter
  */
 
-$parameters=array();
-$reshook=$hookmanager->executeHooks('doActions',$parameters,$object,$action);    // Note that $action and $object may have been modified by some hooks
-if ($reshook < 0) {
+$parameters = array();
+$reshook = $hookmanager->executeHooks('doActions', $parameters, $object, $action); // Note that $action and $object may have been modified by some hooks
+if ($reshook<0) {
 	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
 }
 
 if (empty($reshook))
 {
-	$error=0;
+	$error = 0;
 
 	if ($cancel)
 	{
 		if ($action != 'addlink')
 		{
-			$urltogo=$backtopage?$backtopage:dol_buildpath('/flightbaloon/list.php',1);
+			$urltogo = $backtopage ? $backtopage : dol_buildpath('/flightbaloon/list.php', 1);
 			header("Location: ".$urltogo);
 			exit;
 		}
-		if ($id > 0 || ! empty($ref)) {
+		if ($id>0 || !empty($ref)) {
 			$ret = $object->fetch($id);
 		}
-		$action='';
+		$action = '';
 	}
 
 	// Action to add record
-	if ($action == 'add' && ! empty($user->rights->bal->add))
+	if ($action == 'add' && !empty($user->rights->bal->add))
 	{
 		foreach ($object->fields as $key => $val)
 		{
-			if (in_array($key, array('rowid', 'entity', 'date_creation', 'tms', 'import_key'))) continue;	// Ignore special fields
+			if (in_array($key, array('rowid', 'entity', 'date_creation', 'tms', 'import_key'))) continue; // Ignore special fields
 
-			$object->$key=GETPOST($key,'alpha');
+			$object->$key = GETPOST($key, 'alpha');
 			if ($val['notnull'] && $object->$key == '')
 			{
 				$error++;
-				setEventMessages($langs->trans("ErrorFieldRequired",$langs->transnoentitiesnoconv($val['label'])), null, 'errors');
+				setEventMessages($langs->trans("ErrorFieldRequired", $langs->transnoentitiesnoconv($val['label'])), null, 'errors');
 			}
 		}
 
-		if (! $error)
+		if (!$error)
 		{
-			$result=$object->create($user);
-			if ($result > 0)
+			$result = $object->create($user);
+			if ($result>0)
 			{
 				// Creation OK
-				$urltogo=$backtopage?$backtopage:dol_buildpath('/flightballoon/list.php',1);
+				$urltogo = $backtopage ? $backtopage : dol_buildpath('/flightballoon/list.php', 1);
 				header("Location: ".$urltogo);
 				exit;
 			} else
 			{
 				// Creation KO
-				if (! empty($object->errors)) {
+				if (!empty($object->errors)) {
 					setEventMessages(null, $object->errors, 'errors');
 				} else {
 					setEventMessages($object->error, null, 'errors');
 				}
-				$action='create';
+				$action = 'create';
 			}
 		} else
 		{
-			$action='create';
+			$action = 'create';
 		}
 	}
 
 	// Action to update record
-	if ($action == 'update' && ! empty($user->rights->hello->create))
+	if ($action == 'update' && !empty($user->rights->hello->create))
 	{
 		foreach ($object->fields as $key => $val)
 		{
-			$object->$key=GETPOST($key,'alpha');
+			$object->$key = GETPOST($key, 'alpha');
 			if (in_array($key, array('rowid', 'entity', 'date_creation', 'tms', 'import_key'))) continue;
 			if ($val['notnull'] && $object->$key == '')
 			{
 				$error++;
-				setEventMessages($langs->trans("ErrorFieldRequired",$langs->transnoentitiesnoconv($val['label'])), null, 'errors');
+				setEventMessages($langs->trans("ErrorFieldRequired", $langs->transnoentitiesnoconv($val['label'])), null, 'errors');
 			}
 		}
 
-		if (! $error)
+		if (!$error)
 		{
-			$result=$object->update($user);
-			if ($result > 0)
+			$result = $object->update($user);
+			if ($result>0)
 			{
-				$action='view';
+				$action = 'view';
 			} else
 			{
 				// Creation KO
-				if (! empty($object->errors)) {
+				if (!empty($object->errors)) {
 					setEventMessages(null, $object->errors, 'errors');
 				} else {
 					setEventMessages($object->error, null, 'errors');
 				}
-				$action='edit';
+				$action = 'edit';
 			}
 		} else
 		{
-			$action='edit';
+			$action = 'edit';
 		}
 	}
 
 	// Action to delete
-	if ($action == 'confirm_delete' && ! empty($user->rights->hello->delete))
+	if ($action == 'confirm_delete' && !empty($user->rights->hello->delete))
 	{
-		$result=$object->delete($user);
-		if ($result > 0)
+		$result = $object->delete($user);
+		if ($result>0)
 		{
 			// Delete OK
 			setEventMessages("RecordDeleted", null, 'mesgs');
-			header("Location: ".dol_buildpath('/hello/llx_bbc_ballons_list.php',1));
+			header("Location: ".dol_buildpath('/hello/llx_bbc_ballons_list.php', 1));
 			exit;
 		} else
 		{
-			if (! empty($object->errors)) {
+			if (!empty($object->errors)) {
 				setEventMessages(null, $object->errors, 'errors');
 			} else {
 				setEventMessages($object->error, null, 'errors');
@@ -251,14 +265,18 @@ if ($action == 'create')
 	dol_fiche_head(array(), '');
 
 	print '<table class="border centpercent">'."\n";
-	foreach($object->fields as $key => $val)
+	foreach ($object->fields as $key => $val)
 	{
-		if (in_array($key, array('rowid', 'entity', 'date_creation', 'tms', 'import_key'))) continue;
+		if (in_array($key, array('rowid', 'entity', 'date_creation', 'tms', 'import_key'))) {
+			continue;
+		}
 		print '<tr><td';
 		print ' class="titlefieldcreate';
-		if ($val['notnull']) print ' fieldrequired';
+		if ($val['notnull']) {
+			print ' fieldrequired';
+		}
 		print '"';
-		print '>'.$langs->trans($val['label']).'</td><td><input class="flat" type="text" name="'.$key.'" value="'.(GETPOST($key,'alpha')?GETPOST($key,'alpha'):'').'"></td></tr>';
+		print '>'.$langs->trans($val['label']).'</td><td><input class="flat" type="text" name="'.$key.'" value="'.(GETPOST($key, 'alpha') ?GETPOST($key, 'alpha') : '').'"></td></tr>';
 	}
 	print '</table>'."\n";
 
@@ -311,13 +329,13 @@ if ($object->id>0 && (empty($action) || ($action != 'edit' && $action != 'create
 
 	// Confirmation to delete
 	if ($action == 'delete') {
-		$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"] . '?id=' . $object->id, $langs->trans('DeleteOrder'), $langs->trans('ConfirmDeleteOrder'), 'confirm_delete', '', 0, 1);
+		$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"].'?id='.$object->id, $langs->trans('DeleteOrder'), $langs->trans('ConfirmDeleteOrder'), 'confirm_delete', '', 0, 1);
 	}
 
 	// Confirmation of action xxxx
 	if ($action == 'xxx')
 	{
-		$formquestion=array();
+		$formquestion = array();
 		/*
 	        $formquestion = array(
 	            // 'text' => $langs->trans("ConfirmClone"),
@@ -325,14 +343,14 @@ if ($object->id>0 && (empty($action) || ($action != 'edit' && $action != 'create
 	            // array('type' => 'checkbox', 'name' => 'update_prices', 'label' => $langs->trans("PuttingPricesUpToDate"), 'value' => 1),
 	            // array('type' => 'other',    'name' => 'idwarehouse',   'label' => $langs->trans("SelectWarehouseForStockDecrease"), 'value' => $formproduct->selectWarehouses(GETPOST('idwarehouse')?GETPOST('idwarehouse'):'ifone', 'idwarehouse', '', 1)));
 	    }*/
-		$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"] . '?id=' . $object->id, $langs->trans('XXX'), $text, 'confirm_xxx', $formquestion, 0, 1, 220);
+		$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"].'?id='.$object->id, $langs->trans('XXX'), $text, 'confirm_xxx', $formquestion, 0, 1, 220);
 	}
 
-	if (! $formconfirm) {
+	if (!$formconfirm) {
 		$parameters = array('lineid' => $lineid);
 		$reshook = $hookmanager->executeHooks('formConfirm', $parameters, $object, $action); // Note that $action and $object may have been modified by hook
-		if (empty($reshook)) $formconfirm.=$hookmanager->resPrint;
-		elseif ($reshook > 0) $formconfirm=$hookmanager->resPrint;
+		if (empty($reshook)) $formconfirm .= $hookmanager->resPrint;
+		elseif ($reshook>0) $formconfirm = $hookmanager->resPrint;
 	}
 
 	// Print form confirm
@@ -383,14 +401,14 @@ if ($object->id>0 && (empty($action) || ($action != 'edit' && $action != 'create
 	// Buttons for actions
 	if ($action != 'presend' && $action != 'editline') {
 		print '<div class="tabsAction">'."\n";
-		$parameters=array();
-		$reshook=$hookmanager->executeHooks('addMoreActionsButtons',$parameters,$object,$action);    // Note that $action and $object may have been modified by hook
-		if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
+		$parameters = array();
+		$reshook = $hookmanager->executeHooks('addMoreActionsButtons', $parameters, $object, $action); // Note that $action and $object may have been modified by hook
+		if ($reshook<0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
 
 		if (empty($reshook))
 		{
 			// Send
-			print '<div class="inline-block divButAction"><a class="butAction" href="' . $_SERVER["PHP_SELF"] . '?id=' . $object->id . '&action=presend&mode=init#formmailbeforetitle">' . $langs->trans('SendByMail') . '</a></div>'."\n";
+			print '<div class="inline-block divButAction"><a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=presend&mode=init#formmailbeforetitle">'.$langs->trans('SendByMail').'</a></div>'."\n";
 
 			if ($user->rights->hello->write)
 			{
@@ -417,9 +435,9 @@ if ($object->id>0 && (empty($action) || ($action != 'edit' && $action != 'create
 		print '<a name="builddoc"></a>'; // ancre
 		// Documents
 		$comref = dol_sanitizeFileName($object->ref);
-		$relativepath = $comref . '/' . $comref . '.pdf';
-		$filedir = $conf->hello->dir_output . '/' . $comref;
-		$urlsource = $_SERVER["PHP_SELF"] . "?id=" . $object->id;
+		$relativepath = $comref.'/'.$comref.'.pdf';
+		$filedir = $conf->hello->dir_output.'/'.$comref;
+		$urlsource = $_SERVER["PHP_SELF"]."?id=".$object->id;
 		$genallowed = $user->rights->hello->creer;
 		$delallowed = $user->rights->hello->supprimer;
 		print $formfile->showdocuments('hello', $comref, $filedir, $urlsource, $genallowed, $delallowed, $object->modelpdf, 1, 0, 0, 28, 0, '', '', '', $soc->default_lang);
@@ -433,7 +451,7 @@ if ($object->id>0 && (empty($action) || ($action != 'edit' && $action != 'create
 		print '</div><div class="fichehalfright"><div class="ficheaddleft">';
 
 		// List of actions on element
-		include_once DOL_DOCUMENT_ROOT . '/core/class/html.formactions.class.php';
+		include_once DOL_DOCUMENT_ROOT.'/core/class/html.formactions.class.php';
 		$formactions = new FormActions($db);
 		$somethingshown = $formactions->showactions($object, 'order', $socid);
 
